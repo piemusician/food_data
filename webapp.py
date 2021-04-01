f103cb467a0f15cf8fdc1870a6a5647f8a5d3607
Melany Nunez <melanynunez@Melanys-MacBook-Pro.local>
2021-04-01 15:05:04 -0700

    about page finished

--- webapp.py
+++ webapp.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, Markup, render_template, flash
+from datetime import datetime
 
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_about():
+    return render_template('about.html')
+    
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True) # change to False when running on heroku
